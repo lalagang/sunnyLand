aeea1b566f4d7bb9f3cc73d24f95ee17b963c226
lalagang <caozhiyuankiva@gmail.com>
2022-08-01 21:29:44 +0900

    初回プッシュ

---
+++ Sunnyland/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Input.GetAxis("Horizontal")* 15f * Time.deltaTime, 0f, 0f);
+
+        Vector3 characterScale = transform.localScale;
+
+        if (Input.GetAxis("Horizontal")  < 0)
+        {
+            characterScale.x = -10;
+        }
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 10;
+        }
+        transform.localScale = characterScale;
+    }
+}
---
+++ Sunnyland/Assets/camera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Playerの部分はカメラが追いかけたいオブジェクトの名前をいれる
+        this.player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(playerPos.x, playerPos.y + 2, transform.position.z);
+
+		//if (transform.position.x < -9)
+		//{
+		//	transform.position = new Vector3(-9, transform.position.y, transform.position.z);
+		//}
+
+		//if (playerPos.x < -9)
+		//{
+		//	playerPos.x = -9;
+		//}
+	}
+}
